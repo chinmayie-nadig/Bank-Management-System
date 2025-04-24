09ad37934e5ff793e3c35c85eb29074eb721f7c4
chinmayie-nadig <139335380+chinmayie-nadig@users.noreply.github.com>
2025-04-24 18:32:46 +0530

    Deposit.java

--- Deposit.java
+++ Deposit.java
@@ -15,7 +15,7 @@ public class Deposit extends JFrame implements ActionListener {
     Deposit(String pin) {
         this.pin = pin;
 
-        // Properly scaled background image
+        
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("bank/icons/atm.jpg"));
         Image i2 = i1.getImage().getScaledInstance(960, 750, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
@@ -23,20 +23,20 @@ public class Deposit extends JFrame implements ActionListener {
         background.setBounds(0, 0, 960, 750);
         add(background);
 
-        // Label
+        
         l1 = new JLabel("ENTER AMOUNT YOU WANT TO DEPOSIT");
         l1.setForeground(Color.WHITE);
         l1.setFont(new Font("System", Font.BOLD, 16));
         l1.setBounds(190, 250, 400, 35);
         background.add(l1);
 
-        // TextField
+    
         t1 = new JTextField();
         t1.setFont(new Font("Raleway", Font.BOLD, 22));
         t1.setBounds(190, 300, 320, 25);
         background.add(t1);
 
-        // Buttons
+    
         b1 = new JButton("DEPOSIT");
         b1.setBounds(390, 370, 150, 35);
         background.add(b1);
@@ -48,11 +48,11 @@ public class Deposit extends JFrame implements ActionListener {
         b1.addActionListener(this);
         b2.addActionListener(this);
 
-        // Frame config
+        
         setLayout(null);
-        setSize(960, 750); // updated height to match typical screen
-        setLocationRelativeTo(null); // center window
-        setUndecorated(false); // or true for cleaner frame
+        setSize(960, 750); 
+        setLocationRelativeTo(null); 
+        setUndecorated(false); 
         setVisible(true);
     }
 
